2bafa2eedd0e2fea2357ba82124a977d1560aa72
salviega <salviega6@gmail.com>
2023-09-02 18:15:47 -0500

    :poop: Smart contract incompleted

--- contracts/MeetdApp.sol
+++ contracts/MeetdApp.sol
@@ -1,4 +1,23 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.19;
 
-contract MeetdApp {}
+import '@openzeppelin/contracts/utils/Counters.sol';
+
+contract MeetdApp {
+	using Counters for Counters.Counter;
+
+	Counters.Counter private _eventIdCounter;
+
+	struct Event {
+		uint id;
+		string name;
+		string description;
+		string location;
+		uint date;
+		uint time;
+		uint price;
+		uint totalTickets;
+		uint remainingTickets;
+		address payable owner;
+	}
+}
