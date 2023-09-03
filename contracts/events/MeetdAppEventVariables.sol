539fde65417ea9b1efa70e45860d232efadf8366
salviega <salviega6@gmail.com>
2023-09-02 19:25:14 -0500

    :poop: Bad smart contracts implementations

---
+++ contracts/events/MeetdAppEventVariables.sol
@@ -0,0 +1,15 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.19;
+
+contract MeetdAppEventVariables {
+	uint id;
+	string name;
+	string description;
+	string location;
+	uint date;
+	uint time;
+	uint price;
+	uint totalTickets;
+	uint remainingTickets;
+	address payable owner;
+}
